a55dafacadedf04e7cde23897d1c5d88ba17be10
nishi_hiroyuki <nishi_hiroyuki@cyberagent.co.jp>
2025-01-03 18:56:54 +0900

    fix print

--- route-planner-backend/app/main_test.py
+++ route-planner-backend/app/main_test.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from pprint import pprint
 
 from main import app
 
@@ -16,4 +17,4 @@ def test_get_route():
         json={"start_location_name": "樟葉駅", "prompt": "10km圏内のルートを提示してください。"},
     )
     assert response.status_code == 200
-    print(response.json())
+    pprint(response.json())
